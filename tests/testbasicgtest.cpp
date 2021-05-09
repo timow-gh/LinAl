f498b33811399adc6a25e12bdb2d695bf427207c
timow-gh <39085531+timow-gh@users.noreply.github.com>
2021-05-09 22:39:06 +0200

    Refactoring the directory structure.

--- tests/testbasicgtest.cpp
+++
@@ -1,6 +0,0 @@
-#include "gtest/gtest.h"
-
-TEST(basicGTest, testEqual) { EXPECT_EQ(1, 1); }
-TEST(basicGTest, testTrue) { EXPECT_TRUE(true); }
-
-TEST(basicGTest, testFalse) { EXPECT_FALSE(false); }
