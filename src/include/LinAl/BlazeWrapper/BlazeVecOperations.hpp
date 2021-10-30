408d08b984cae236ea21ee7efd0b1c189351616e
timow-gh <39085531+timow-gh@users.noreply.github.com>
2021-10-30 16:22:49 +0200

    Added README.md and License.

--- src/include/LinAl/BlazeWrapper/BlazeVecOperations.hpp
+++ src/include/LinAl/BlazeWrapper/BlazeVecOperations.hpp
@@ -10,19 +10,19 @@ namespace LinAl
 {
 
 template <typename T, std::size_t D>
-[[nodiscard]] T dot(const Vec<T, D>& lhs, const Vec<T, D>& rhs)
+[[nodiscard]] constexpr T dot(const Vec<T, D>& lhs, const Vec<T, D>& rhs)
 {
     return blaze::dot(lhs, rhs);
 }
 
 template <typename T>
-[[nodiscard]] Vec3<T> cross(const Vec3<T>& lhs, const Vec3<T>& rhs)
+[[nodiscard]] constexpr Vec3<T> cross(const Vec3<T>& lhs, const Vec3<T>& rhs)
 {
     return blaze::cross(lhs, rhs);
 }
 
 template <typename T, std::size_t D>
-[[nodiscard]] T norm2Squared(const Vec<T, D>& vector)
+[[nodiscard]] constexpr T norm2Squared(const Vec<T, D>& vector)
 {
     T sum{0};
     for (const T& value: vector)
@@ -31,33 +31,33 @@ template <typename T, std::size_t D>
 }
 
 template <typename T, std::size_t D>
-[[nodiscard]] T norm2(const Vec<T, D>& vector)
+[[nodiscard]] constexpr T norm2(const Vec<T, D>& vector)
 {
     return blaze::norm(vector);
 }
 
 template <typename T, std::size_t D>
-[[nodiscard]] Vec<T, D> normalize(const Vec<T, D>& vector)
+[[nodiscard]] constexpr Vec<T, D> normalize(const Vec<T, D>& vector)
 {
     return Vec<T, D>{blaze::normalize(vector)};
 }
 
 template <typename T, std::size_t D>
-[[nodiscard]] Vec<T, D> projection(const Vec<T, D>& source,
+[[nodiscard]] constexpr Vec<T, D> projection(const Vec<T, D>& source,
                                    const Vec<T, D>& target)
 {
     return target * (dot(source, target)) / (dot(target, target));
 }
 
 template <typename T, std::size_t D>
-[[nodiscard]] Vec<T, D> rejection(const Vec<T, D>& source,
+[[nodiscard]] constexpr Vec<T, D> rejection(const Vec<T, D>& source,
                                   const Vec<T, D>& target)
 {
     return source - projection(source, target);
 }
 
 template <typename T, std::size_t D>
-[[nodiscard]] bool collinear(const Vec<T, D>& source, const Vec<T, D>& target)
+[[nodiscard]] constexpr bool collinear(const Vec<T, D>& source, const Vec<T, D>& target)
 {
     return Core::isZero(T(1) - std::abs(LinAl::dot(LinAl::normalize(source),
                                                    LinAl::normalize(target))));
@@ -68,7 +68,7 @@ struct isNormalized{};
 // clang-format on
 
 template <typename T, std::size_t D>
-[[nodiscard]] bool
+[[nodiscard]] constexpr bool
 collinear(const Vec<T, D>& source, const Vec<T, D>& target, isNormalized)
 {
     return Core::isZero(T(1) - std::abs(LinAl::dot(source, target)));
