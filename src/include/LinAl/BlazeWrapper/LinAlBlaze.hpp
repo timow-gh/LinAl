240f75cb06d4629b1ef1eca389550e25b22c2730
timow-gh <39085531+timow-gh@users.noreply.github.com>
2022-04-29 20:10:37 +0200

    Don't use GCC diagnostic when compiling with msvc.

--- src/include/LinAl/BlazeWrapper/LinAlBlaze.hpp
+++ src/include/LinAl/BlazeWrapper/LinAlBlaze.hpp
@@ -2,10 +2,16 @@
 #define GLFWTESTAPP_BLAZE_HPP
 
 //save gcc compiler switches
+#if !defined(_MSVC_LANG)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wpedantic"
+#endif
+
 #include <blaze/Blaze.h>
+
+#if !defined(_MSVC_LANG)
 //restore compiler switches
 #pragma GCC diagnostic pop
+#endif
 
 #endif // GLFWTESTAPP_BLAZE_HPP
