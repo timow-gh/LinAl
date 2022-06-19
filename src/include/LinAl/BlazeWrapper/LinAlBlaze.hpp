ae7aa7dd8527e7d2a254f0ab51af6a157067048d
timow-gh <39085531+timow-gh@users.noreply.github.com>
2022-06-19 16:32:52 +0200

    Ignore blaze warnings.

--- src/include/LinAl/BlazeWrapper/LinAlBlaze.hpp
+++ src/include/LinAl/BlazeWrapper/LinAlBlaze.hpp
@@ -1,17 +1,10 @@
 #ifndef GLFWTESTAPP_BLAZE_HPP
 #define GLFWTESTAPP_BLAZE_HPP
 
-//save gcc compiler switches
-#if !defined(_MSVC_LANG)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wpedantic"
-#endif
-
+#include <Core/Utils/Warnings.h>
+DISABLE_ALL_WARNINGS
 #include <blaze/Blaze.h>
+ENABLE_ALL_WARNINGS
 
-#if !defined(_MSVC_LANG)
-//restore compiler switches
-#pragma GCC diagnostic pop
-#endif
 
 #endif // GLFWTESTAPP_BLAZE_HPP
